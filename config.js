301ae7411e7396574a03f79e0058207c9f13c195
luism9601 <107263505+luism9601@users.noreply.github.com>
2023-11-29 06:56:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.botname = 'SARAH'
-global.premium = 'true'
+global.premium = 'false'
 global.packname = 'SARAH' 
 global.author = '@Asliguru' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
